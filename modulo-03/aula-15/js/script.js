af2a769b436d6a7bbc59dca4b430079da80bd39c
cuaitz <ocuait@gmail.com>
2026-04-23 22:20:14 -0300

    [FEAT] Final da aula 15

--- modulo-03/aula-15/js/script.js
+++ modulo-03/aula-15/js/script.js
@@ -1,13 +1,31 @@
 const lista = document.querySelector("#tarefas");
 const input = document.querySelector("#input");
+const busca = document.querySelector("#busca");
 
 // o nome disso é template literal; talvez js não seja tão ruim assim...
 const tarefaTemplate = (texto) => {
-    return `<li>${texto}</li>`;
+    return `<li>${texto}
+        <button>X</button>
+    </li>`;
 }
 
 document.querySelector("#tarefaForm").addEventListener('submit', (e) => {
     e.preventDefault();
     lista.innerHTML += (tarefaTemplate(input.value));
     input.value = '';
+});
+
+lista.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') { // caps lock por algum motivo
+        e.target.parentElement.remove();
+    }
+});
+
+busca.addEventListener('input', (e) => {
+    let termo = e.target.value.toLowerCase();
+    let elementos = lista.querySelectorAll('li');
+
+    for (const item of elementos) {
+        item.style.display = item.textContent.includes(termo) ? '' : 'none';
+    }
 });
